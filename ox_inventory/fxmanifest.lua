c5dc528c82bc632ec2d0d45a830e430c5e88c4e6
HiTTA <92121743+hittascripts@users.noreply.github.com>
2025-04-20 18:29:14 +0200

    Add files via upload

---
+++ ox_inventory/fxmanifest.lua
@@ -0,0 +1,52 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+name 'ox_inventory'
+author 'Overextended'
+version '2.44.1'
+repository 'https://github.com/overextended/ox_inventory'
+description 'Slot-based inventory with item metadata support'
+
+dependencies {
+    '/server:6116',
+    '/onesync',
+    'oxmysql',
+    'ox_lib',
+}
+
+shared_script '@ox_lib/init.lua'
+
+ox_libs {
+    'locale',
+    'table',
+    'math',
+}
+
+server_scripts {
+    'logs.lua',
+    '@oxmysql/lib/MySQL.lua',
+    'init.lua'
+}
+
+client_scripts {
+    'logs.lua',
+    'init.lua',
+}
+
+ui_page 'web/build/index.html'
+
+files {
+    'logs.lua',
+    'client.lua',
+    'server.lua',
+    'locales/*.json',
+    'web/build/index.html',
+    'web/build/assets/*.js',
+    'web/build/assets/*.css',
+    'web/images/*.png',
+    'modules/**/shared.lua',
+    'modules/**/client.lua',
+    'modules/bridge/**/client.lua',
+    'data/*.lua',
+}
---
+++ ox_inventory/logs.lua
@@ -0,0 +1,42 @@
+Config = {}
+
+
+-- ###################################  WEBHOOKS LOGS #######################################
+
+
+Config.Staff_Spawn       = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Deposit_Car       = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Deposit_Safe      = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Deposit_Glovebox  = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Deposit_Player    = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Deposit_Floor     = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Remove_Car        = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Remove_Safe       = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Remove_Glovebox   = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Robbery_Player    = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Remove_Floor      = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Give_Player       = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Upgrade_Gun       = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Downgrade_Gun     = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+Config.Shopping_Store    = 'https://discord.com/api/webhooks/1363353813226098829/b5UwlNdqv6RjJcZcFBidczWDbHrPgZmTkyOWOz04dQXmR7gp9uAOERYvk4DRQ6ysU2Bd'
+
+
+
+-- ###################################  LOG TITLES ############################################
+
+
+Config.DepositSafe     = '☆☆☆ STASH DEPOSIT ☆☆☆'
+Config.RemoveSafe      = '☆☆☆ STASH WITHDRAW ☆☆☆'
+Config.DepositCarTrunk = '☆☆☆ TRUNK DEPOSIT ☆☆☆'
+Config.RemoveCarTrunk  = '☆☆☆ TRUNK WITHDRAW ☆☆☆'
+Config.DepositGlovebox = '☆☆☆ GLOVEBOX DEPOSIT ☆☆☆'
+Config.RemoveGlovebox  = '☆☆☆ GLOVEBOX WITHDRAW ☆☆☆'
+Config.DepositPlayer   = '☆☆☆ GIVE PLAYER ☆☆☆'
+Config.RobberyPlayer   = '☆☆☆ ROB PLAYER ☆☆☆'
+Config.DepositFloor    = '☆☆☆ DROP FLOOR ☆☆☆'
+Config.RemoveFloor     = '☆☆☆ TAKE FLOOR ☆☆☆'
+Config.GivePlayer      = '☆☆☆ GIVE PLAYER ☆☆☆'
+Config.GiveStaff       = '☆☆☆ GIVE STAFF ☆☆☆'
+Config.DowngradeGun    = '☆☆☆ DOWNGRADE GUN ☆☆☆'
+Config.UpgradeGun      = '☆☆☆ UPGRADE GUN ☆☆☆'
+Config.ShoppingStore   = '☆☆☆ STORE ☆☆☆'
